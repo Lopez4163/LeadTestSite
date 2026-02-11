999e9cbca550595c2fff92abc1b5b8c5d3848233
Nicholas Lopez <nicholas@gesture.vip>
2026-02-11 09:58:27 -0500

    added script to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <script 
+        id="vtag-ai-js" 
+        async src="https://r2.leadsy.ai/tag.js" 
+        data-pid="fNSoodsh5UqSsFOY" 
+        data-version="062024">
+      </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
